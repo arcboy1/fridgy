7b0209cd31e20f06d3c60cfb51b7f0fc733643d9
Noah T <116191639+arcboy1@users.noreply.github.com>
2024-09-25 22:00:52 -0400

    created a button with a pickerview as the subview which will act as a filter bar for food items in the fridge - previous commit didnt stage the changes for some reason

--- FridgyApp/ViewController.swift
+++ FridgyApp/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: OUTLETS
 
+    @IBOutlet weak var filterButton: UIButton!
+    
+    //MARK: PROPERTIES
+    
+    //instance of UIPickerView
+    let pickerView = UIPickerView()
+
+    let filterOptions = ["All Items", "Drinks", "Condiments", "Food", "Snacks", "Fruit","Vegetable","Meat","Dairy","Dessert","Other"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        filterButton.addTarget(self, action: #selector(showPicker), for: .touchUpInside)
+        
+
     }
+    
+    @objc func showPicker() {
+            // creates alert to show picker as action
+            let alertController = UIAlertController(title: "Select Filter", message: "\n\n\n\n\n\n", preferredStyle: .actionSheet)
+            
+            // add the pickerView as a subview of the alert
+            pickerView.frame = CGRect(x: 0, y: 50, width: alertController.view.bounds.width, height: 150)
+            alertController.view.addSubview(pickerView)
+            
+            
+            alertController.addAction(UIAlertAction(title: "Done", style: .default, handler: { _ in
+                let selectedRow = self.pickerView.selectedRow(inComponent: 0)
+                self.filterButton.setTitle(self.filterOptions[selectedRow], for: .normal) // Set the selected option in the button title
+            }))
+            
+            present(alertController, animated: true, completion: nil)
+        }
 
 
 }
-
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //methods for supplying data to pickerview and controlling its appearance/text
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return filterOptions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return filterOptions[row]
+    }
+}
